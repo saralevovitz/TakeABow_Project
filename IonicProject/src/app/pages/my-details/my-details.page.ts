0eee2a6dc9f471704017bf16fa007460c0299542
MichalVahav <michalivahav@gmail.com>
2021-01-01 00:33:48 +0200

    michal0101

--- IonicProject/src/app/pages/my-details/my-details.page.ts
+++ IonicProject/src/app/pages/my-details/my-details.page.ts
@@ -24,7 +24,7 @@ export class MyDetailsPage implements OnInit {
   }
   
   ngOnInit() {
-  localStorage.setItem()  
+  //localStorage.setItem()  
    // this.id =+ localStorage.getItem('userId')
     console.log(this.getDetails())
   }
